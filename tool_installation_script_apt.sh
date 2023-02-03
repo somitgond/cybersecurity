f77f463fda933f8847bf417036963a3794b85093
Somit Gond <62469008+somitgond@users.noreply.github.com>
2023-02-03 17:07:51 +0530

    Update tool_installation_script_apt.sh

--- tool_installation_script_apt.sh
+++ tool_installation_script_apt.sh
@@ -1,2 +1,2 @@
-sudo apt update && sudo apt upgrade -y && sudo apt install -y gobuster wfuzz wget curl net-tools python3 pip g++ binwalk exiftool docker.io docker-compose neo-vim wireshark \
+sudo apt update && sudo apt upgrade -y && sudo apt install -y wget curl net-tools python3 pip g++ binwalk exiftool docker.io docker-compose neo-vim wireshark \
 openvpn openssh
